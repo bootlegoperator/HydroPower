3559c32ef43e7c5bc6316a8413473bd3e10f0f50
bootlegoperator <54648848+bootlegoperator@users.noreply.github.com>
2019-08-28 20:11:47 -0400

    Removing annoying vsfile thing

--- src/HydroPower.java
+++ src/HydroPower.java
@@ -7,6 +7,6 @@ public class HydroPower {
     * @param args arguments for starting the program
     */
     public static void main(String[] args) {
-        System.out.println("lmao");
+        System.out.println("Testing... should run");
     }
 }
