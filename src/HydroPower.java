16d7bf9d2749e8caf1375f2fcc4866f13a057164
bootlegoperator <54648848+bootlegoperator@users.noreply.github.com>
2019-08-29 20:45:07 -0400

    Added functions to calculate power output

--- src/HydroPower.java
+++ src/HydroPower.java
@@ -6,7 +6,26 @@ public class HydroPower {
     * Entrypoint of the program
     * @param args arguments for starting the program
     */
+
+
+    // DENSITY = mass/volume
+
+
+    public static final float PI = 3.14159265359f;
+    public static final float GRAVITY = 9.81f;
+
+
+    public static double WaterFlow(double tubeDiameter, double waterVelocity)
+    {
+        return PI*tubeDiameter*tubeDiameter*velocity/4.0;
+    }
+
+    public static double PowerOutput(double density, double waterFlow, double fallingHeight)
+    {
+        return density*waterFlow*fallingHeight*9.81;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Testing... should run");
+
     }
 }
